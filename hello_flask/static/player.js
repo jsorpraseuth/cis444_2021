895f777e5fad7dbf357050f8f98f79627f9885dc
jsorp <sorpr002@cougars.csusm.edu>
2021-10-08 14:48:06 -0700

    fixed css

--- hello_flask/static/player.js
+++ hello_flask/static/player.js
@@ -11,29 +11,33 @@ var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
 music.addEventListener("timeupdate", timeUpdate, false);
 
 function timeUpdate() {
-	var playPercent = timelineWidth * (music.currentTime / duration);
-	playhead.style.width = playPercent + "px";
+   var playPercent = timelineWidth * (music.currentTime / duration);
+   playhead.style.width = playPercent + "px";
 
-	var secondsIn = Math.floor(((music.currentTime / duration) / 3.5) * 100);
-	if (secondsIn <= 9) {
-		timer.innerHTML = "0:0" + secondsIn;
-	} else {
-		timer.innerHTML = "0:" + secondsIn;
-	}
+   var secondsIn = Math.floor((music.currentTime / duration / 3.5) * 100);
+   if (secondsIn <= 9) {
+      timer.innerHTML = "0:0" + secondsIn;
+   } else {
+      timer.innerHTML = "0:" + secondsIn;
+   }
 }
 
-playButton.onclick = function() {
-	music.play();
-	playButton.style.visibility = "hidden";
-	pause.style.visibility = "visible";
-}
+playButton.onclick = function () {
+   music.play();
+   playButton.style.visibility = "hidden";
+   pause.style.visibility = "visible";
+};
 
-pauseButton.onclick = function() {
-	music.pause();
-	playButton.style.visibility = "visible";
-	pause.style.visibility = "hidden";
-}
+pauseButton.onclick = function () {
+   music.pause();
+   playButton.style.visibility = "visible";
+   pause.style.visibility = "hidden";
+};
 
-music.addEventListener("canplaythrough", function () {
-	duration = music.duration;
-}, false);
+music.addEventListener(
+   "canplaythrough",
+   function () {
+      duration = music.duration;
+   },
+   false
+);
