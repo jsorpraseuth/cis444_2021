02a05aa0ab321ea2be8b450d3db96b6d89c0c540
jsorp <sorpr002@cougars.csusm.edu>
2021-10-26 15:39:03 -0700

    fix "inconsistent tabs and spaces"

--- hello_flask/db_a3.py
+++ hello_flask/db_a3.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_db():
 	return psycopg2.connect(host="localhost", dbname="books", user="root", password="root")
-
+	
 def get_db_instance():  
-    db = get_db()
-    cur = db.cursor()
+	db = get_db()
+	cur = db.cursor()
 	
 	return db, cur
