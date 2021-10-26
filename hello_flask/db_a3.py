6352c6ecabc20b07819f108830cff50b35fb07f7
jsorp <sorpr002@cougars.csusm.edu>
2021-10-26 15:37:18 -0700

    what is wrong with the spacing aws

--- hello_flask/db_a3.py
+++ hello_flask/db_a3.py
@@ -1,12 +1,7 @@
 import psycopg2
 
 def get_db():
-	return psycopg2.connect(
-		host="localhost", 
-		dbname="books", 
-		user="root", 
-		password="root"
-	)
+	return psycopg2.connect(host="localhost", dbname="books", user="root", password="root")
 
 def get_db_instance():  
     db = get_db()
