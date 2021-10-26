dc590188441fe72ecc01a3149fd2f2325c699064
jsorp <sorpr002@cougars.csusm.edu>
2021-10-26 15:51:33 -0700

    figure out why no web page is appearing

--- hello_flask/a3.py
+++ hello_flask/a3.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request
-from flask_json import FlaskJSON
-from db_a3 import get_db
+#from flask_json import FlaskJSON
+#from db_a3 import get_db
 
-import bcrypt
+#import bcrypt
 
 app = Flask(__name__)
-FlaskJSON(app)
+#FlaskJSON(app)
 
-JWT_SECRET = None
+#JWT_SECRET = None
 
-global_db_con = get_db()
+#global_db_con = get_db()
 
-with open("secret", "r") as f:
-    JWT_SECRET = f.read()
+#with open("secret", "r") as f:
+#    JWT_SECRET = f.read()
 
 @app.route('/')	#endpoint
 def index():
