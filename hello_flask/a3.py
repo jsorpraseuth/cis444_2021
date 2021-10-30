0bfc765141bed32395fc14f4aa3ed95e9f7ec35b
jsorp <sorpr002@cougars.csusm.edu>
2021-10-30 16:42:57 -0700

    completely redo login :l

--- hello_flask/a3.py
+++ hello_flask/a3.py
@@ -1,108 +1,57 @@
 from flask import Flask, render_template, request
 from flask_json import FlaskJSON, json_response
-from flask.json import jsonify
-from db_a3 import get_db, get_db_instance
+from db_a3 import get_db
 
+import json
 import jwt
 import bcrypt
-import json
-
-IMGS_URL = {
-            "DEV" : "/static",
-            "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
-            "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
-            }
-
-CUR_ENV = "PRD"
+import datetime
 
 app = Flask(__name__)
 FlaskJSON(app)
 
+TOKEN = None
+SECRET = None
+
+with open("secret", "r") as f:
+    SECRET = f.read()
+
 # postgres db
 db = get_db()
 
-# declare token
-TOKEN = None
-JWT_SECRET = None
-
-with open("secret", "r") as f:
-    JWT_SECRET = f.read()
-
 #----------------------------------------#
-# Default Page
+# Index page
 #----------------------------------------#
-
 # login page and store page
 @app.route('/')	# default endpoint
-def index():
-    return render_template('index.html')
-
-#----------------------------------------#
-# Token
-#----------------------------------------#
-
-# logout
-@app.route('/logout', methods=['GET'])
-def logout():
-	global TOKEN
-	TOKEN = None
-	print("User logged out successfully.")
-	return json_response(data={"message": "Logged out successfully."})
-
-
-#----------------------------------------#
-# Books
-#----------------------------------------#
-
-
-
-#----------------------------------------#
-# Account Creation and Verification
-#----------------------------------------#
+def home():
+	return 
 	
-# create user credentials
-@app.route('/create', methods=['POST'])
-def create():
-	cur = db.cursor()
-	form = request.form
-	# call database to see if user exists
-	cur.execute("SELECT * FROM users WHERE username = '" + jwt.encode({'username':form['username']}, JWT_SECRET, algorithm="HS256") + "';")
-	# if username is available, create credentials
-	if cur.fetchone() is None:
-		user = jwt.encode({'username':form['username']}, JWT_SECRET, algorithm="HS256")
-		encrypted_pass = bcrypt.hashpw(bytes(form['password'], 'utf-8'), bcrypt.gensalt(11))
-		cur.execute("INSERT INTO users (username, password, created_on) values ('" + user + "', '" + encrypted_pass + "', current_timestamp);")
-		# important commit created user to db
-		db.commit()
-		print('User "' + form['username'] + '" created successfully.')
-		return json_response(data={"message": "User created successfully."})
-	else:
-		print('Error: "' + form['username'] + '" already in use.')
-		return json_response(data={"message": "Username is already in use."})
+#----------------------------------------#
+# User Verification
+#----------------------------------------#
 
-# verify user credentials	
-@app.route('/verify', methods=['POST'])
-def verify():
+# checks if username is taken, adds user to database
+@app.route('/signup', methods=["POST"])
+def signup():
 	cur = db.cursor()
-	form = request.form
-	user = jwt.encode({'username':form['username']}, JWT_SECRET, algorithm="HS256")
-	# call database to see if user exists
-	cur.execute("SELECT * FROM users WHERE USERNAME = '" + user + "';")
-	# stackoverflow fix :]
-	row = cur.fetchone()
-	# if user does not exists
-	if row is None:
-		print('Error: "' + form['username'] + '" does not exist.')
-		return json_response(data={"message": "Username does not exist."})
-	# check password
+	
+	try:
+		cur.execute("select * from users where username = '" + request.form["username"] + "';")
+	except:
+		return json_response(data = {"error" : "Database could not be accessed."}, status = 500)
+		
+	if cur.fetchOne() is None:
+		saltedPassword = bcrypt.hashpw(bytes(request.form["password"], "utf-8"), bcrypt.gensalt(11))
+		try:
+			cur.execute("insert into users (username, password, created_on) values '" + request.form["username"] + "', " + saltedPassword.decode("utf-8") + "', current_timestamp;")
+			db.commit()
+			print("Created user '" + request.form["username"] + "'.")
+			return json_response(data = {"success" : "User created successfully."})
+		except:
+			return json_response(data = {"error" : "Could not reach database to create user."})
 	else:
-		if bcrypt.checkpw(bytes(form['password'], 'utf-8'), bytes(row[2], 'utf-8')) == True:
-			print('User "' + form['username'] + '" successful login.')
-			global TOKEN
-			TOKEN = jwt.encode({"user_id": row[0]}, JWT_SECRET, algorithm="HS256")
-			return json_response(data={"jwt": TOKEN})
-		else:
-			print("Incorrect password input.")
-			return json_response(data={"message": "Incorrect Password. Please try again."})
+		print("Username '" + request.form["username"] + "' taken,")
+		return json_response(data = {"error" : "Username '" + request.form["username"] + "' already in use."
 
-app.run(host='0.0.0.0', port=80)
+app.run(host = '0.0.0.0', port = 80)
