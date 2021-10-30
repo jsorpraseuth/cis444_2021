9695fe356e1d5d28c94fec71a6b3396489d6d412
jsorp <sorpr002@cougars.csusm.edu>
2021-10-30 14:41:45 -0700

    revert

--- hello_flask/a3.py
+++ hello_flask/a3.py
@@ -48,70 +48,12 @@ def logout():
 	print("User logged out successfully.")
 	return render_template("index.html", verification="Logged out successfully.")
 
-def validateToken(newToken):
-    global TOKEN, JWT_SECRET
-
-    if TOKEN is None:
-        print("No token exists.")
-        return False
-    else:
-        fromServer = jwt.decode(TOKEN, JWT_SECRET, algorithms=["HS256"])
-        fromRequest = jwt.decode(newToken, JWT_SECRET, algorithms=["HS256"])
-
-        if fromServer == fromRequest:
-            print("Valid token.")
-            return True
-        else:
-            print("Tokens do not match.")
-            return False
 
 #----------------------------------------#
 # Books
 #----------------------------------------#
 
-@app.route('/getBooks', methods=['POST'])
-def getbooks():
-	if validateToken(request.form["jwt"]):
-		cur = db.cursor()
-		try:
-			cur.execute("select * from books;")
-		except:
-			return json_reponse(data={"message": "Could not retrieve books from database."}, status=500)
-		
-		count = 0
-		message = '{"books:['
-		while 1:
-			row = cur.fetchone()
-			if row is None:
-				break
-			else:
-				if count > 0:
-					message += ","
-					
-				message += '{"book_id":' + str(row[0]) + ',"title:"' + str(row[1]) + ',"author:"' + str(row[2]) + ',"genre:"' + str(row[3]) + ',"price:"' + str(row[4]) + "}"
-				count += 1
-			
-			message += "]}"
-			
-			print("Sending list of books")
-			return json_response(data=json.loads(message))
-		else:
-			print("Invalid token.")
-			return json_response(data={"message": "User is not logged in." }, status=404)
-			
-@app.route('/buyBook', methods=['POST'])
-def buyBook():
-	global JWT_SECRET
-	decodedUser = jwt.decode(request.form["jwt"], JWT_SECRET, algorithms=["HS256"])
-	cur = db.cursor()
-	
-	try:
-		cursor.execute("insert into purchases (user_id, book_id, purchased_on) values ('" + str(decodedUser["user_id"]) + "', '" + str(request.form["book_id"]) + "', current_timestamp);'")
-		db.commit()
-		print("Purchase success. Sending message.")
-		return json_response(data={"message": "Book bought successfully."})
-	except:
-		return json_response(data={"message": "Error occured while writing to database."}, status=500)
+
 
 #----------------------------------------#
 # Account Creation and Verification
