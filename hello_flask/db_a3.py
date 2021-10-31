46f5f2e34892ff93f6eb30050913a2a3467c6048
jsorp <sorpr002@cougars.csusm.edu>
2021-10-30 17:20:05 -0700

    Update db_a3.py

--- hello_flask/db_a3.py
+++ hello_flask/db_a3.py
@@ -4,7 +4,6 @@ def get_db():
 	return psycopg2.connect(host="localhost", dbname="books", user="root", password="root")
 	
 def get_db_instance():  
-db  = get_db()
-cur  = db.cursor()
-
-return db, cur 
+	db  = get_db()
+	cur  = db.cursor()
+	return db, cur 
