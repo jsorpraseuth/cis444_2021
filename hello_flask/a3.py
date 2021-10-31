3455054b8cd065a58062da9e2c72ad61d04a92e4
jsorp <sorpr002@cougars.csusm.edu>
2021-10-30 20:54:11 -0700

    hard code it temporarily until ask jardin

--- hello_flask/a3.py
+++ hello_flask/a3.py
@@ -108,7 +108,7 @@ def loadBooks():
 		try:
 			# grab books from db
 			#cur.execute("select * from books;")
-			cur.execute(f"select * from books where not exists (select from purchases where books.book_id = purchases.book_id and user_id = '{user_id}');")
+			cur.execute("select * from books where not exists (select from purchases where books.book_id = purchases.book_id and user_id = 5);")
 			print("Grabbed books from database that were not purchased by user.")
 		except:
 			print("Could not find books from database.")
