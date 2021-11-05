ab311407b62979c04b004c7daf73f198448a4355
jsorp <sorpr002@cougars.csusm.edu>
2021-11-05 16:39:55 -0700

    Update db_con.py

--- hello_flask/db_con.py
+++ hello_flask/db_con.py
@@ -1,16 +1,14 @@
 import psycopg2
 
 # class example
-#def get_db():
-#    return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
+def get_db():
+    return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
 
 def get_db_instance():  
     db  = get_db()
     cur  = db.cursor( )
 
     return db, cur 
-
-
 
 if __name__ == "__main__":
     db, cur = get_db_instance()
