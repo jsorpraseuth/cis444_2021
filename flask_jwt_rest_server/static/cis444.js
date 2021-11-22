a12016464ad2d01b8c53d0dfec60474fb9217264
jsorp <sorpr002@cougars.csusm.edu>
2021-11-21 22:59:16 -0800

    Update cis444.js

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -111,9 +111,12 @@ function get_books() {
 }
 
 function buyBook(id) {
-	secure_get_with_token("/secure_api/buy_book", {"id" : id} , function(data) {
+	book_id = id;
+	
+	secure_get_with_token("/secure_api/buy_book", {"id" : book_id} , function(data) {
 		console.log("buy book"); 
-		console.log("book id: " + id);
+		console.log("book id 1: " + id);
+		console.log("book id 2: " + book_id);
 		console.log(data);
 		
 		alert(data.message);
