8f475b43ab39aa2606d777cf8267ae34a9e3e676
jsorp <sorpr002@cougars.csusm.edu>
2021-11-21 21:42:06 -0800

    Update token_required.py

--- flask_jwt_rest_server/tools/token_required.py
+++ flask_jwt_rest_server/tools/token_required.py
@@ -30,6 +30,8 @@ def token_required(f):
 			token = auth_headers[1]
 			logger.debug("Got token")
 			logger.debug("Attempting to decode the token: " + token)
+			logger.debug("Current g.jwt_data value: " + g.jwt_data)
+			logger.debug("Current data value: " + data)
 			data = jwt.decode(token, secrets['JWT'], algorithms = ["HS256"])
 			logger.debug("Decoded token: " + data)
 			#set global jwt_data
