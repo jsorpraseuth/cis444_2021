9ccd9d5674cd3204dbd0b7ef38bebc2c441b5716
jsorp <sorpr002@cougars.csusm.edu>
2021-11-21 22:51:09 -0800

    Update get_books.py

--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -10,15 +10,13 @@ import simplejson as json
 def handle_request():
 	logger.debug("Get Books Handle Request")
 	cur = g.db.cursor()	
-	user = g.jwt_data['sub']
 
-	query = sql.SQL("select * from {table} where not exists (select from {table2} where books.book_id = purchases.book_id and username = {username});").format(
+	query = sql.SQL("select * from {table} where not exists (select from {table2} where books.book_id = purchases.book_id and username = %s);").format(
 		table = sql.Identifier('books'),
 		table2 = sql.Identifier('purchases'),
-		username = user
 	)
 	
-	cur.execute(query)
+	cur.execute(query. (g.jwt_data['sub']))
 	print("Grabbed books from database that were not purchased by user.")
 	db_books = cur.fetchall()
 	
