c95243ce349fc766f33eb39a3c70a19e15292767
jsorp <sorpr002@cougars.csusm.edu>
2021-11-21 22:18:28 -0800

    Update get_books.py

--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -9,13 +9,15 @@ import simplejson as json
 
 def handle_request():
 	logger.debug("Get Books Handle Request")
-	cur = g.db.cursor()
-	
-	query = sql.SQL("select * from {table};").format(
+	cur = g.db.cursor()	
+
+	query = sql.SQL("select * from {table} where no exists (select from {table2} where books.book_id = purchases.book_id and user_id = 1;").format(
 		table = sql.Identifier('books')
+		table2 = sql.Identifier('purchases')
 	)
 	
 	cur.execute(query)
+	print("Grabbed books from database that were not purchased by user.")
 	db_books = cur.fetchall()
 	
 	return json_response(token = create_token(g.jwt_data), books = db_books)
