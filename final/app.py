e4ea45d58ad0e5a4a21bc234f4f0a6370b271632
james <sorpr002@cougars.csusm.edu>
2021-12-13 15:26:47 -0800

    Add temperature check and api route

--- final/app.py
+++ final/app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+API_KEY = 'deeb10dd2998cb3ea5ddf7240b75c918'
 
 @app.route('/')
 def index():
     return '<h1>Welcome to weather app</h1>'
+
+@app.route('/city')
+def search_by_city():
+    key = API_KEY
+    city = request.args.get('q')
+
+    # call API
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+    response = requests.get(url).json()
+
+    # exception handling
+    if response.get('cod') != 200:
+        message = response.get('message', '')
+        return f'Error getting temperature for {city.title()}. Error message = {message}'
+
+    # get temperature
+    current_temperature = response.get('main', {}).get('temp')
+    if current_temperature:
+        current_temperature_celsius = round(current_temperature - 273.15, 2)
+        return f'Current temperature of {city.title()} is {current_temperature} &#8457;, {current_temperature_celsius} &#8451;'
+    else:
+        return f'Error getting temperature for {city.title()}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
