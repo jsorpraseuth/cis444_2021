43b8d6270e370336ca1534c47131f6427013863b
james <sorpr002@cougars.csusm.edu>
2021-12-13 16:49:47 -0800

    prep front end

--- final/app.py
+++ final/app.py
@@ -12,9 +12,17 @@ def index():
 def search_by_city():
     key = 'deeb10dd2998cb3ea5ddf7240b75c918'
     city = request.args.get('city')
+    data = get_results(city, key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
 
+    return render_template('weather.html', location=location, weather=weather, feels_like=feels_like, temp=temp)
+
+def get_results(city, api):
     # call API
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api}'
     response = requests.get(url).json()
 
     # exception handling
@@ -22,15 +30,7 @@ def search_by_city():
         message = response.get('message', '')
         return f'Error getting temperature for {city.title()}. Error message = {message}'
 
-    # get temperature
-    current_temperature = response.get('main', {}).get('temp')
-    if current_temperature:
-        current_temperature_fahrenheit = round((current_temperature - 273.15) * 9/5 + 32, 2)
-        current_temperature_celsius = round(current_temperature - 273.15, 2)
-        return f'Current temperature of {city.title()} is {current_temperature_fahrenheit} &#8457;, {current_temperature_celsius} &#8451;'
-    else:
-        return f'Error getting temperature for {city.title()}'
-
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
